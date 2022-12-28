133fe58aadfc252096246e6a92d2f78c34d5ace2
Muslim24-09 <Leonard76@mail.ru>
2022-12-28 21:36:51 +0300

    thirteen fixed statuses 2

--- controllers/users.js
+++ controllers/users.js
@@ -3,13 +3,13 @@ const User = require('../models/user');
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(200).send({ data: users }))
-    .catch((err) => res.status(500).send({ message: `Error: ${err} "Запрашиваемые пользователи не найдены"` }));
+    .catch((err) => res.status(400).send({ message: `Error: ${err} "Запрашиваемые пользователи не найдены"` }));
 };
 
 const getUserById = (req, res) => {
   User.findById(req.params.userId)
     .then((user) => res.status(200).send({ data: user }))
-    .catch((err) => res.status(500).send({ message: `Error: ${err} - "Запрашиваемый пользователь не найден"` }));
+    .catch((err) => res.status(400).send({ message: `Error: ${err} - "Запрашиваемый пользователь не найден"` }));
 };
 
 const createUser = (req, res) => {
