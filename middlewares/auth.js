ba38418ef8b23cc92b21c455db8e4845943e8c0a
Muslim24-09 <Leonard76@mail.ru>
2023-01-09 23:47:52 +0300

    fourteen to fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,16 +6,15 @@ const handleAuthError = (res) => {
     .send({ message: 'Необходима авторизация' });
 };
 
-const extractBearerToken = (header) => header.replace('Bearer ', '');
-
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
+  // иначе не работает авторизация. Причину не нашел
+  const token = req.rawHeaders.find((el) => el.match('jwt')).replace('jwt=', '');
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return handleAuthError(res);
-  }
+  // if (!authorization || !authorization.startsWith('Bearer ')) {
+  //   return handleAuthError(res);
+  // }
 
-  const token = extractBearerToken(authorization);
+  // const token = extractBearerToken(authorization);
   let payload;
 
   try {
