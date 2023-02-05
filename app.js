55dff1461a7785759f028c18020c6c6db06a249a
Muslim24-09 <Leonard76@mail.ru>
2023-02-05 20:05:12 +0300

    crash-test added

--- app.js
+++ app.js
@@ -25,6 +25,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
