86d242040a42b6762e712df989e1d87a687fee93
Muslim24-09 <Leonard76@mail.ru>
2023-02-09 22:57:45 +0300

    searching bugs 2.0

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,10 +5,8 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, _, next) => {
-  // убрать консоль
-  console.log(req);
   if (!req.headers.cookie) {
-    throw new UnauthorizedError('Требуется авторизация');
+    throw new UnauthorizedError('111 Требуется авторизация');
   }
 
   const token = req.headers.cookie.replace('jwt=', '');
@@ -17,7 +15,7 @@ module.exports = (req, _, next) => {
   try {
     payload = jwt.verify(token, `${NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'}`);
   } catch (err) {
-    throw new UnauthorizedError('Требуется авторизация');
+    throw new UnauthorizedError('222 Требуется авторизация');
   }
   req.user = payload;
 
