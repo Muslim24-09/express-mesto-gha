ddb3bf60702c380c108cb71e2087c9f8f7a06686
Muslim24-09 <Leonard76@mail.ru>
2023-02-14 19:08:33 +0300

    cors added again

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,29 +1,29 @@
-// const allowedCors = [
-//   'https://mooslim-mesto.nomoredomainsclub.ru',
-//   'http://mooslim-mesto.nomoredomainsclub.ru',
-//   'http://localhost:3000',
-// ];
+const allowedCors = [
+  'https://mooslim-mesto.nomoredomainsclub.ru',
+  'http://mooslim-mesto.nomoredomainsclub.ru',
+  'http://localhost:3000',
+];
 
-// const cors = (req, res, next) => {
-//   const { origin } = req.headers;
-//   const { method } = req;
-//   const requestHeaders = req.headers['access-control-request-headers'];
-//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+const cors = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//     res.header('Access-Control-Allow-Credentials', 'true');
-//   }
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
 
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
 
-//     return res.end();
-//   }
+    return res.end();
+  }
 
-//   return next();
-// };
+  return next();
+};
 
 // const cors = (req, res, next) => {
 //   const { origin } = req.headers;
@@ -83,4 +83,4 @@
 //   return next();
 // };
 
-// module.exports = cors;
+module.exports = cors;
